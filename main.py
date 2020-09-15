ba762a5f2b5f148afd4aff05ba7dcc9b6a808425
Michael Camden-Smith <mcamdensmith@gmail.com>
2020-09-15 14:57:51 +0200

    formatting

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from user_input_functions import input_and_validation_from_options
 from rules_logic import rules_logic_pre_dealer, rules_logic_post_dealer
 from components import Player, Dealer, Deck, Card
 
-
 # Game settings
 number_of_decks = 6
 blackjack_multiplier = 1.5
 game_rounds = 1000
-
 
 def main(game_deck, dealer, player): 
     for i in range(1, game_rounds + 1):
@@ -27,7 +25,6 @@ def main(game_deck, dealer, player):
         player.reset()
         game_deck.reset()
         print('----- Round Finished -----'.format(i))
-
 
 if __name__ == '__main__':  
     # Initialize game deck, dealer and player
--- user_input_functions.py
+++ user_input_functions.py
@@ -6,11 +6,12 @@ def input_and_validation_from_options(list_of_options):
         short_option = option[start:end]
         player_options_shorthand.append(short_option)
     while True:
-        print("Please enter your selection from the following options (use single letters) {}: ".format(list_of_options))
+        print("Please enter your selection from the following options (use single letters) {}: ".format(
+            list_of_options))
         selection = str(input())
         if selection.lower() not in player_options_shorthand:
             print("Invalid response")
             continue
         else:
             break
-    return selection    
+    return selection
